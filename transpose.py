4de46118ddeb41beab1dbd7ba812965c58badde7
MONOJIT PALIT <monojitpalit4@gmail.com>
2022-06-17 01:50:06 +0530

    Create transpose.py

---
+++ transpose.py
@@ -0,0 +1,8 @@
+# finding the transpose of a matrix
+class Solution(object):
+    def rotate(self, matrix):
+        for i in range(len(matrix)):
+            for j in range(i, len(matrix[0])):
+                temp = matrix[i][j]
+                matrix[i][j] = matrix[j][i]
+                matrix[j][i] = temp
