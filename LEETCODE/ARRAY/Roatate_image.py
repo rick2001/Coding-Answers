b7c8aa8191a7c066d5cb7e4f864fed4f634d30d2
MONOJIT PALIT <monojitpalit4@gmail.com>
2022-08-06 17:16:54 +0530

    Create Roatate_image.py

---
+++ LEETCODE/ARRAY/Roatate_image.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def rotate(self, matrix):
+        for i in range(len(matrix)):
+            for j in range(i, len(matrix[0])):
+                temp = matrix[i][j]
+                matrix[i][j] = matrix[j][i]
+                matrix[j][i] = temp
+        for k in range(len(matrix)):
+            matrix[k].reverse()
+        return 
