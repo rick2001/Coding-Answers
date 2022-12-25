c7e19f359e9b31381e445cdb1a8476e4cb41716a
MONOJIT PALIT <monojitpalit4@gmail.com>
2022-12-26 02:34:25 +0530

    Update print_all_subsequences2.py

--- Recursion Understanding Striver/print_all_subsequences2.py
+++ Recursion Understanding Striver/print_all_subsequences2.py
@@ -1,6 +1,6 @@
 # print all the subsequences whose summation is "k";
 
-
+# way-1
 def recursion_on_subsequences(index,array,empty_array, final_arr, count,k):
     if index == len(array):
         if count == k:
@@ -24,6 +24,39 @@ count=0
 k=int(input("Enter the Target Element-> "))
 recursion_on_subsequences(index,array,empty_array, final_arr, count,k)
 print(final_arr)
+
+#OUTPUT
+#Enter the Target Element-> 2
+# [[1, 1], [2]]
+
+
+
+
+# way-2
+
+def recursion_on_subsequences(index,array,empty_array, final_arr,target):
+    if index == len(array):
+        if  target == 0:
+            final_arr.append(empty_array.copy())
+        return
+    if array[index] <=target:
+        empty_array.append(array[index])
+        recursion_on_subsequences(index + 1, array, empty_array, final_arr, target-array[index])
+        empty_array.pop()
+    recursion_on_subsequences(index+1,array,empty_array, final_arr,target)
+
+    
+    
+    
+index = 0
+array = [1, 2, 1]
+empty_array = []
+final_arr=[]
+# count=0
+target = int(input("Enter the Target Element-> "))
+recursion_on_subsequences(index,array,empty_array, final_arr, target)
+print(final_arr)
+
 
 #OUTPUT
 #Enter the Target Element-> 2
