ee660c86660f071bfe31a60249aee37ac2563187
MONOJIT PALIT <monojitpalit4@gmail.com>
2023-01-28 13:09:17 +0530

    Update 19. Remove Nth Node From End of List.c

--- LEETCODE/Linkedlist/19. Remove Nth Node From End of List.c
+++ LEETCODE/Linkedlist/19. Remove Nth Node From End of List.c
@@ -1,3 +1,58 @@
+// In both python and c language are written this code
+// In Python Language
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        #Most Optimized Approach
+        dummyHead = ListNode(0)
+        dummyHead.next = head
+        slow=dummyHead
+        fast=dummyHead
+        
+        for i in range(1, n+1):
+            fast=fast.next
+        
+        while fast.next != None:
+            slow = slow.next
+            fast = fast.next
+        slow.next = slow.next.next
+        return dummyHead.next
+        
+         
+        #BruteForce Approach
+#         dummyHead1 = head
+#         dummyHead2 = head
+#         count=0
+#         while dummyHead1!=None:   #Find the length
+#             count+=1
+#             dummyHead1=dummyHead1.next
+#         count = count - n
+#         print(count)
+#         if count == 0: #it means from last head node is instructed to be deleted
+#             return head.next
+#         else:
+#             for i in range(count-1):
+#                 dummyHead2 = dummyHead2.next
+#             dummyHead2.next = dummyHead2.next.next
+#         return head
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// In c Language
 /**
  * Definition for singly-linked list.
  * struct ListNode {
