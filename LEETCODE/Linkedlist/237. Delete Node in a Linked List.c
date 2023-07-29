8ba50ceda61feeec8af75ba2152aaef4f9222ddd
MONOJIT PALIT <monojitpalit4@gmail.com>
2023-07-29 21:56:52 +0530

    237. Delete Node in a Linked List.c

--- LEETCODE/Linkedlist/237. Delete Node in a Linked List.c
+++ LEETCODE/Linkedlist/237. Delete Node in a Linked List.c
@@ -1,3 +1,22 @@
+// code is avilable in both c and java
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val=node.next.val;
+        node.next=node.next.next;
+    }
+}
+
+
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
