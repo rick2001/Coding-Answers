734964d3ef876d51aabcec795090565f147b9a63
MONOJIT PALIT <monojitpalit4@gmail.com>
2023-07-31 00:57:23 +0530

    160. Intersection of Two Linked Lists.c

--- LEETCODE/Linkedlist/160. Intersection of Two Linked Lists.c
+++ LEETCODE/Linkedlist/160. Intersection of Two Linked Lists.c
@@ -1,3 +1,42 @@
+// code is avilable in both c and java. 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+// this is java code
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode tempA=headA;
+        ListNode tempB=headB;
+        
+        while(tempA!=tempB){
+            if(tempA==null){
+                tempA=headB;
+            }
+            else{
+                tempA=tempA.next;    
+            }
+            if(tempB==null){
+                tempB=headA;
+            }
+            else{
+                tempB=tempB.next;    
+            }
+            
+        }
+        return tempA;
+    }
+}
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
