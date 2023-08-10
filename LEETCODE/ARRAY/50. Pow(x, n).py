6ef0e8de75bfb49f16c5f8346edf2a6d7bc61dc3
MONOJIT PALIT <monojitpalit4@gmail.com>
2023-08-11 01:48:01 +0530

    50. Pow(x, n).py

--- LEETCODE/ARRAY/50. Pow(x, n).py
+++ LEETCODE/ARRAY/50. Pow(x, n).py
@@ -1,3 +1,32 @@
+# code is available in both java and python
+# JAVA
+class Solution {
+    public double myPow(double x, int n) {
+        if(x==0) return 0;
+        if(n==0) return 1;
+        double ans=1.0;
+        long nn=n;  //taking it long as temp variable because if n is negative (The range is -2,147,483,647), positive conversion of n cant be stored in int type.
+        if(n<0) nn=-1 * nn;  // convert it into positive value
+        
+        while(nn > 0){
+            if(nn%2==0){   // if n is even number
+                x=x*x;
+                nn= nn/2;
+            }
+            else{          // if n is odd number
+                ans=ans*x;
+                nn=nn-1;
+            }
+        }
+        if(n<0) ans= (double)1.0/(double)ans;   //id n is negative then perform 1/x^n;
+        return ans;
+            
+        
+    }
+}
+
+
+# PYTHON
 class Solution(object):
     def myPow(self, x, n):
         #basic solution
